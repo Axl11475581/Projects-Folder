8b4ab47ab8ac8513b2b2658e45d5f9d96ea11026
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-11 10:43:18 -0500

    Variable Types in comment format

--- C# Exercises/Intro.cs
+++ C# Exercises/Intro.cs
@@ -7,8 +7,15 @@ public class Intro : MonoBehaviour
     // Start is called before the first frame update
     void Start ()
     {
-
+        
     }
+        // Variable Type:
+        
+        // <scope accesor><data type><variable name> = <value>;
+        // scope - public, private, protected
+        // data type - int, float, string, char, bool, Vector 2, Vector 3, GameObject, Trandform
+        // variable - canBeAnything, health, jumpSpeed, isDead
+        // value - bool - true, false, char 'a', string "a statement inside", int 23, float 23.43f
 
     // Update is called once per frame
     void Update ()
