3108cc8437c2eda33cae694bf25137fb1dd951d7
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-12 17:03:39 -0500

    Following the Python tutorial

---
+++ Python Exercises/Projects.py
@@ -0,0 +1,34 @@
+# 1 exercise, print command:
+print("Hello World!\n")
+
+# 2 exercise, string variables
+
+person_name = "Tom"
+object_1 = "Car"
+object_2 = "House"
+Date = "23 of September"
+
+print("Hello " + person_name)
+print("Do you have a " + object_1 + "?")
+print("And when are you moving to your new " + object_2)
+print("I hope is before the wedding that we have been invited to on " + Date)
+print("\n")
+
+# 3 data storage
+
+sentence = "I love pizza and nachos"
+
+print(sentence.upper().isupper())
+print("\n")
+
+# 4 number manipulation
+
+print(min(8,2,3,4,5,89))
+print("\n")
+
+# 5 number calculation
+
+print(round(52/36+pow(5,3)))
+print("\n")
+
+# 6 
