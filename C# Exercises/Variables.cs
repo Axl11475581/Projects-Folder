fa8f811ed41971b24de66f57d08c557c90c3c737
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-12 09:09:10 -0500

    New local location for the repository and new file called 'Variables'

---
+++ C# Exercises/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            int count = 10;
+            float totalPrice = 20.99f;
+            char character = 'A';
+            string firstName = "Mash";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+        }
+    }
+}
