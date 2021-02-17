0393cb1d2045e95b3a8900fc5f5d7f64f60ec2c0
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-17 10:16:46 -0500

    exercise with functions and loops

---
+++ Python Exercises/Python Basics/Exercise Functions.py
@@ -0,0 +1,11 @@
+# Write a function that evaluates a list of values and prints the highest even number
+
+def highest_even(li):
+    evens = []
+    for item in li:
+        if item % 2 == 0:
+            evens.append(item)
+    return max(evens)
+
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
