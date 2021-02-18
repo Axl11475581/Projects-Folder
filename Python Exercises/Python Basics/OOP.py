a5de024001d8d854d965343291028234095096d7
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-18 17:46:46 -0500

    OOP Exercise #2

--- Python Exercises/Python Basics/OOP.py
+++ Python Exercises/Python Basics/OOP.py
@@ -68,3 +68,53 @@ def oldest_cat(*args):
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 print(f"The oldest cat is {oldest_cat(Bianca.age, Max.age, Lulu.age)} years old.")
+
+
+# OOP Exercise 2
+
+class Pets():
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+# 1 Add another Cat
+
+class Bianca(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+# 2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = [Simon('Simon', 4), Sally('Sally', 21), Bianca('Bianca', 6)]
+
+# 3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
+# 4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
