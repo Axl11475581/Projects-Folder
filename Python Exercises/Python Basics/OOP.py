4d52377f882174d2cd97365294ba5527ed5dab40
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-18 16:12:52 -0500

    Completed the OOP Exercise

--- Python Exercises/Python Basics/OOP.py
+++ Python Exercises/Python Basics/OOP.py
@@ -62,7 +62,9 @@ Lulu = Cat('Lulu', 2)
 
 
 # 2 Create a function that finds the oldest cat
-def oldest_cat():
-    pass
+def oldest_cat(*args):
+    return max(args)
+
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f"The oldest cat is {oldest_cat(Bianca.age, Max.age, Lulu.age)} years old.")
