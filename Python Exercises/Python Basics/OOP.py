dded524ee3db6be208ed29d6d0743fa87bac96f9
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-18 10:04:58 -0500

    more comments

--- Python Exercises/Python Basics/OOP.py
+++ Python Exercises/Python Basics/OOP.py
@@ -12,5 +12,6 @@ print(type({}))
 What is an object? Objects have methods like these and attributes
 that you can access with the DOT method. Now OOP allows the programmer to 
 go beyond what Python just gives us (these data types) and create out own.
-
+Why is this useful? OOP is a paradigm, it's a way for us to think about our
+code and structure in a way that is easier to maintain, extend and write.
 """
