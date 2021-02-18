38d11220ee1d90f8b46cdd6893377e699d6f20cc
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-18 10:01:33 -0500

    Class on OOP

---
+++ Python Exercises/Python Basics/OOP.py
@@ -0,0 +1,16 @@
+# OOP
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+
+"""
+What is an object? Objects have methods like these and attributes
+that you can access with the DOT method. Now OOP allows the programmer to 
+go beyond what Python just gives us (these data types) and create out own.
+
+"""
