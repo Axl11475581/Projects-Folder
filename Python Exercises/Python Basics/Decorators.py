ed9cada4d0c8be94e3e4940473ffaec9e945999c
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-19 14:14:10 -0500

    First part of learning about decorators

---
+++ Python Exercises/Python Basics/Decorators.py
@@ -0,0 +1,5 @@
+# Decorators work due to the ability of functions to act like variables, to act like first class citizens in Python
+# In short, decorators supercharge our functions, add extra functionality to them
+
+# Also we need to understand the idea of Higher Order Function
+
