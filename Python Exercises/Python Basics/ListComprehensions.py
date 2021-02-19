8babc5e3044c14a32da6dd438200b619e27791f6
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-19 12:13:57 -0500

    Update ListComprehensions.py

--- Python Exercises/Python Basics/ListComprehensions.py
+++ Python Exercises/Python Basics/ListComprehensions.py
@@ -11,5 +11,14 @@ print(my_list)
 
 # One can use List Comprehension to get the same result:
 my_list2 = [char for char in 'hello']
+my_list3 = [num for num in range(0, 100)]
+my_list4 = [num*2 for num in range(0, 100)]
 
 print(my_list2)
+print(my_list3)
+print(my_list4)
+
+# One can have different functions and checkers inside the [] to do complex iterations in one line
+my_list5 = [num**2 for num in range(0, 100) if num % 2 == 0]
+
+print(my_list5)
