1579dfe8f3bdcd3da0ebe26451ae03536a9608fb
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-19 15:23:52 -0500

    definition of Higher Order Functions

--- Python Exercises/Python Basics/Decorators.py
+++ Python Exercises/Python Basics/Decorators.py
@@ -2,4 +2,15 @@
 # In short, decorators supercharge our functions, add extra functionality to them
 
 # Also we need to understand the idea of Higher Order Function
+# A HOC can be a function that accepts another function:
+def greet(func):
+    func()
+    pass
 
+
+# Or also a function that returns another function:
+def greet2():
+    def func():
+        return 5
+
+    return func()
