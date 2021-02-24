0938db20a34d8682e8d1daf4ee0a5753dca927f6
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-24 11:04:06 -0500

    converting images with python

--- Python Exercises/Scripting with Python/main.py
+++ Python Exercises/Scripting with Python/main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('pikachu.jpg')
-filtered_img = img.filter(ImageFilter.GaussianBlur)
-filtered_img.save("blur.png", 'png')
-print(img)
+# filtered_img = img.filter(ImageFilter.GaussianBlur)
+# filtered_img.save("blur.png", 'png')
+# print(img)
+filtered_img = img.convert('L')
+filtered_img.save("grey.png", 'png')
