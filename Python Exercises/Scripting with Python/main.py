e7d6e23564dc1eb591fe62ee0b5f6da1fccc5d80
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-24 14:57:02 -0500

    resizing the astro image without losing details and quality

--- Python Exercises/Scripting with Python/main.py
+++ Python Exercises/Scripting with Python/main.py
@@ -1,8 +1,20 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('pikachu.jpg')
+# img = Image.open('pikachu.jpg')
 # filtered_img = img.filter(ImageFilter.GaussianBlur)
 # filtered_img.save("blur.png", 'png')
 # print(img)
-filtered_img = img.convert('L')
-filtered_img.save("grey.png", 'png')
+# filtered_img = img.convert('L')
+# filtered_img.save("grey.png", 'png')
+
+img = Image.open('astro.jpg')
+"""
+new_img = img.resize((400, 400))
+new_img.save('astro_thumbnail.jpg')
+"""
+# by resizing that way the image loses details as it gets squish up
+img.thumbnail((400, 200))
+img.save('astro_thumbnail.jpg')
+
+
+
