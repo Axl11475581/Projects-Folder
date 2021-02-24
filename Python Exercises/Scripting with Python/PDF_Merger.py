918c98506b0181c81485a5d1b50de0d4fb835a2f
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-24 17:30:33 -0500

    it merges the PDF file if it lies in the same directory as them, don't know how to make it locate the desire directory just yet

---
+++ Python Exercises/Scripting with Python/PDF_Merger.py
@@ -0,0 +1,15 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('Combine_PDF.pdf')
+
+
+pdf_combiner(inputs)
