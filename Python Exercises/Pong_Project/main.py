e58c9f3c64009c8961b5bec1a2d395324d40934a
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-25 18:44:05 -0500

    better  organized the file and began working on the key input

--- Python Exercises/Pong_Project/main.py
+++ Python Exercises/Pong_Project/main.py
@@ -14,6 +14,7 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
+
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
@@ -22,7 +23,26 @@ paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+# move the paddles with the keyboard
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+# Keyboard binding
+window.listen()
+window.onkeypress(paddle_a_up, "w")
 
 # Main game loop
 while True:
