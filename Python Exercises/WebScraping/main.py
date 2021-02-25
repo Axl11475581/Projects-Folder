c717387dcf5bca2c0b541e27527a78d9650be588
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-25 14:21:06 -0500

    manage to get the title and the link of the newest articles in HN now implementing the votes count

--- Python Exercises/WebScraping/main.py
+++ Python Exercises/WebScraping/main.py
@@ -3,4 +3,17 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/newest')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+links = soup.select('.storylink')
+votes = soup.select('.score')
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        hn.append({'title': title, 'link': href})
+    return hn
+
+
+print(create_custom_hn(links, votes))
