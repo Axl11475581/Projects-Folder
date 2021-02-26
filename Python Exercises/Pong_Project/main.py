16cc296a5426a1932c8f858cdce28af4c5fc8db4
Axl11475581 <77015177+Axl11475581@users.noreply.github.com>
2021-02-25 20:36:06 -0500

    controls for the two paddles complete, now working on the ball movement and screen boundaries

--- Python Exercises/Pong_Project/main.py
+++ Python Exercises/Pong_Project/main.py
@@ -40,9 +40,30 @@ def paddle_a_up():
     paddle_a.sety(y)
 
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
 # Keyboard binding
 window.listen()
 window.onkeypress(paddle_a_up, "w")
+window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_b_up, "Up")
+window.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
